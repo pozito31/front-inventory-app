abf7c60824b75903db3d67abb7981ff4b34e37b4
pozito31 <jessicapozo198715@gmail.com>
2023-02-06 08:53:03 +0100

    actualizando cambios de roles de los usuarios

--- src/app/modules/category/components/category/category.component.ts
+++ src/app/modules/category/components/category/category.component.ts
@@ -6,6 +6,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { CategoryService } from '../../../shared/services/category.service';
 import { NewCategoryComponent } from '../new-category/new-category.component';
 import { ConfirmComponent } from '../../../shared/components/confirm/confirm.component';
+import { UtilService } from 'src/app/modules/shared/services/util.service';
 
 @Component({
   selector: 'app-category',
@@ -14,12 +15,16 @@ import { ConfirmComponent } from '../../../shared/components/confirm/confirm.com
 })
 export class CategoryComponent implements OnInit {
 
+  isAdmin: any;
 
   constructor(private categoryService: CategoryService,
-              public dialog: MatDialog, private snackBar: MatSnackBar) { }
+    public dialog: MatDialog, private snackBar: MatSnackBar,
+    private util: UtilService
+  ) { }
 
   ngOnInit(): void {
     this.getCategories();
+    this.isAdmin = this.util.isAdmin();
   }
   displayedColumns: string[] = ['id', 'name', 'description', 'actions'];
   dataSource = new MatTableDataSource<CategoryElement>();
--- src/app/modules/product/product/product.component.ts
+++ src/app/modules/product/product/product.component.ts
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar, MatSnackBarRef, SimpleSnackBar } from '@angular/material/snack-bar';
 import { NewProductComponent } from '../new-product/new-product.component';
 import { ConfirmComponent } from '../../shared/components/confirm/confirm.component';
+import { UtilService } from '../../shared/services/util.service';
 
 @Component({
   selector: 'app-product',
@@ -14,14 +15,18 @@ import { ConfirmComponent } from '../../shared/components/confirm/confirm.compon
 })
 export class ProductComponent implements OnInit {
 
+  isAdmin: any;
+
   constructor(
     private productService: ProductService,
     public dialog: MatDialog,
     private snackBar: MatSnackBar,
+    private util: UtilService
   ) { }
 
   ngOnInit(): void {
     this.getProducts();
+    this.isAdmin = this.util.isAdmin();
   }
 
   displayedColumns: string[] = ['id', 'name', 'price', 'account', 'category', 'picture',  'actions'];
